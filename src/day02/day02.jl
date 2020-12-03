acd18d4b8ce097bd78fbd97f37be9169142456e0
Alexander Seiler <seileralex@gmail.com>
2020-12-03 03:06:47 +0100

    [Day 2] Simplify

--- src/day02/day02.jl
+++ src/day02/day02.jl
@@ -12,28 +12,15 @@ function day02(input::String = readInput(joinpath(@__DIR__, "input.txt")))
         low = parse(Int, m.captures[1])
         high = parse(Int, m.captures[2])
         char = m.captures[3][1]
-        if _is_valid_part1(low, high, char, m.captures[4])
+        pw = m.captures[4]
+        if low <= (c == char for c in pw) |> sum <= high
             part1 += 1
         end
-        if _is_valid_part2(low, high, char, m.captures[4])
+        if xor(pw[low] == char, pw[high] == char)
             part2 += 1
         end
     end
     return [part1, part2]
-end
-
-function _is_valid_part1(low::Int, high::Int, char::Char, password::AbstractString)
-    n = 0
-    for c in password
-        if c == char
-            n += 1
-        end
-    end
-    return low <= n <= high
-end
-
-function _is_valid_part2(low::Int, high::Int, char::Char, password::AbstractString)
-    return (password[low] == char && password[high] != char) || (password[high] == char && password[low] != char)
 end
 
 end # module
