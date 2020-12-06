c102328968524f2e7206d15f0d90503b82e5f11a
Alexander Seiler <seileralex@gmail.com>
2020-12-06 17:37:00 +0100

    [Day 6] Simplify

--- src/day06/day06.jl
+++ src/day06/day06.jl
@@ -3,9 +3,10 @@ module Day06
 using AdventOfCode2020
 
 function day06(input::String = readInput(joinpath(@__DIR__, "input.txt")))
-    part1 = replace.(split(input, "\n\n"), "\n"=>"") .|> unique .|> length |> sum
+    spinput = split(strip(input), "\n\n")
+    part1 = replace.(spinput, "\n"=>"") .|> unique .|> length |> sum
 
-    inp = split.(split(strip(input), "\n\n"), "\n")
+    inp = split.(spinput, "\n")
     part2 = 0
     for ans in inp
         part2 += intersect(collect.(ans)...) |> length
