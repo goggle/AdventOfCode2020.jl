aaf467c15002d87409fb6423248c5c16a1f1d1ad
Alexander Seiler <seileralex@gmail.com>
2020-12-08 15:41:58 +0100

    [Day 7] Add solution

--- test/runtests.jl
+++ test/runtests.jl
@@ -112,3 +112,27 @@ end
     @test AdventOfCode2020.Day06.day06(sample) == [11, 6]
     @test AdventOfCode2020.Day06.day06() == [6775, 3356]
 end
+
+@testset "Day 7" begin
+    sample = "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" *
+             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" *
+             "bright white bags contain 1 shiny gold bag.\n" *
+             "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" *
+             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" *
+             "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" *
+             "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" *
+             "faded blue bags contain no other bags.\n" *
+             "dotted black bags contain no other bags.\n"
+    @test AdventOfCode2020.Day07.day07(sample) == [4, 32]
+
+    sample2 = "shiny gold bags contain 2 dark red bags.\n" *
+              "dark red bags contain 2 dark orange bags.\n" *
+              "dark orange bags contain 2 dark yellow bags.\n" *
+              "dark yellow bags contain 2 dark green bags.\n" *
+              "dark green bags contain 2 dark blue bags.\n" *
+              "dark blue bags contain 2 dark violet bags.\n" *
+              "dark violet bags contain no other bags.\n"
+    @test AdventOfCode2020.Day07.day07(sample2)[2] == 126
+
+    @test AdventOfCode2020.Day07.day07() == [177, 34988]
+end
