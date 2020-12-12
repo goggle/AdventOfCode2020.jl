543c19a9d9890516830b96e0df6ba34815527c83
Alexander Seiler <seileralex@gmail.com>
2020-12-12 04:50:41 +0100

    [Day 11] Improve

--- src/day11/day11.jl
+++ src/day11/day11.jl
@@ -10,31 +10,43 @@ end
 function solve(seats::Array{Char,2}, occupied_rule::Int, countf)
     state_changed = true
     m, n = size(seats)
+    cseats = collect(seats)
+    nseats = collect(seats)
     while state_changed
         state_changed = false
-        nseats = collect(seats)
         for i = 1:m
             for j = 1:n
-                if seats[i, j] != '.'
-                    c = countf(seats, i, j, '#')
-                    if seats[i, j] == '#' && c >= occupied_rule
+                if cseats[i, j] != '.'
+                    c = countf(cseats, i, j, '#')
+                    if cseats[i, j] == '#' && c >= occupied_rule
                         nseats[i, j] = 'L'
                         state_changed = true
-                    elseif seats[i, j] == 'L' && c == 0
+                    elseif cseats[i, j] == 'L' && c == 0
                         nseats[i, j] = '#'
                         state_changed = true
                     end
                 end
             end
         end
-        seats = nseats
+        copy!(cseats, nseats)
     end
-    return count(i -> (i == '#'), seats)
+    return count(i -> (i == '#'), cseats)
 end
 
 function count_seats_part1(seats::Array{Char,2}, i::Int, j::Int, char::Char)
-    sub = (seats[i, j] == char) ? 1 : 0
-    return count(x -> (x == char), get(seats, (i-1:i+1, j-1:j+1), 'b')) - sub
+    c = 0
+    for Δx in (-1, 0, 1)
+        for Δy in (-1, 0, 1)
+            (Δx == 0 && Δy == 0) && continue
+            x, y = i + Δx, j + Δy
+            !(x >= 1 && x <= size(seats)[1]) && continue
+            !(y >= 1 && y <= size(seats)[2]) && continue
+            if seats[x, y] == char
+                c += 1
+            end
+        end
+    end
+    return c
 end
 
 function count_seats_part2(seats::Array{Char,2}, i::Int, j::Int, char::Char)
