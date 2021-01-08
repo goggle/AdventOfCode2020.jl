9aab7b55360fb50d4e939cc7fea77805f6e22708
Alexander Seiler <seileralex@gmail.com>
2021-01-08 03:36:23 +0100

    [Day 19] Add solution for both parts

--- src/day19/day19.jl
+++ src/day19/day19.jl
@@ -3,82 +3,71 @@ module Day19
 using AdventOfCode2020
 
 function day19(input::String = readInput(joinpath(@__DIR__, "input.txt")))
-    dinit, words = parse_input(input)
+    rules, words = parse_input(input)
 
-    d = replace_numbers(dinit)
-    p1 = 0
-    rule0 = Regex("^" * d[0] * "\$")
-    for word in words
-        if match(rule0, word) != nothing
-            p1 += 1
-        end
+    rule = Array{Union{Int,Char},1}()
+    for r in rules[0][1]
+        push!(rule, r)
     end
+    crule = deepcopy(rule)
 
-    dinit[8] = " 42 | 42 8 "
-    dinit[11] = " 42 31 | 42 11 31 "
-    d = replace_numbers(dinit)
-    # return d
-    p2 = 0
-    rule0 = Regex("^" * d[0] * "\$")
-    for word in words
-        if match(rule0, word) != nothing
-            p2 += 2
-        end
-    end
+    p1 = count(check!(rule, rules, word) for word in words)
+
+    rules[8] = [[42], [42, 8]]
+    rules[11] = [[42, 31], [42, 11, 31]]
+    p2 = count(check!(crule, rules, word) for word in words)
+
     return [p1, p2]
+end
+
+function check!(rule::Array{Union{Int,Char},1}, rules::Dict{Int,Union{Array{Array{Int,1},1},Char}}, word::AbstractString)
+    length(rule) > length(word) && return false
+    all(typeof.(rule) .<: Char) && join(rule) == word && return true
+
+    i = 0
+    while i + 1 < length(rule)
+        if !isinteger(rule[i + 1])
+            i += 1
+            continue
+        end
+        break
+    end
+    i >= 1 && join(rule[1:i]) != word[1:i] && return false
+
+    newrules = Array{Array{Union{Int,Char},1},1}()
+    for (i, r) in enumerate(rule)
+        if isinteger(r)
+            for repl in rules[r]
+                push!(newrules, [rule[1:i-1]..., repl..., rule[i+1:end]...])
+            end
+            return any(check!(newrule, rules, word) for newrule in newrules)
+        end
+    end
+    return false
 end
 
 function parse_input(input::String)
     sinput = split(input, "\n\n")
     p1 = sinput[1]
-    d = Dict{Int,String}()
+    rules = Dict{Int,Union{Array{Array{Int,1},1},Char}}()
     for line in split(p1, "\n")
         sline = split(line, ":")
         key = parse(Int, sline[1])
-        d[key] = replace(sline[2] * " ", "\"" => "")
+        rulestr = replace(sline[2] * " ", "\"" => "")
+        srulestr = split(strip(rulestr), "|")
+        rules[key] = Array{Array{Int,1},1}()
+        for rule in srulestr
+            numbers = split(strip(rule), " ")
+            add = tryparse.(Int, numbers)
+            if nothing ∉ add
+                push!(rules[key], add)
+            else
+                rules[key] = strip(rule)[1]
+            end
+        end
     end
     words = split(rstrip(sinput[2]), "\n")
-
-    return d, words
-end
-
-function replace_numbers(d)
-    # i = 0
-    n = count(contains.(values(d), r"\d+"))
-    m = 0
-    while n != m
-        rl = filter(p->!contains(p.second, r"\d+"), d)
-        for (k, nv) in rl
-            for (k2, v2) in d
-                k2 == 0 && continue
-                numbstr = string(k)
-                d[k2] = replace(d[k2], " " * numbstr * " " => " ( " * nv * " ) ")
-            end
-        end
-        m = n
-        n = count(contains.(values(d), r"\d+"))
-    end
-
-    for i = 1:20
-    #     println("YES")
-    #     c = copy(d)
-        for (k, nv) in d
-            for k2 in (8, 11)
-                numbstr = string(k)
-                d[k2] = replace(d[k2], " " * numbstr * " " => " ( " * nv * " ) ")
-            end
-        end
-    end
-    # c = copy(d)
-    for (k, nv) in d
-        numbstr = string(k)
-        d[0] = replace(d[0], " " * numbstr * " " => " ( " * nv * " ) ")
-    end
-
-    for (k, v) in d
-        d[k] = replace(d[k], r"\s*" => "")
-    end
-    return d
+    return rules, words
 end
 
 end  # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -385,69 +385,70 @@ end
     @test AdventOfCode2020.Day18.day18() == [25190263477788, 297139939002972]
 end
 
-# @testset "Day 19" begin
-#     sample1 = "0: 4 1 5\n" *
-#              "1: 2 3 | 3 2\n" *
-#              "2: 4 4 | 5 5\n" *
-#              "3: 4 5 | 5 4\n" *
-#              "4: \"a\"\n" *
-#              "5: \"b\"\n\n" *
-#              "ababbb\n" *
-#              "bababa\n" *
-#              "abbbab\n" *
-#              "aaabbb\n" *
-#              "aaaabbb\n"
-#     @test AdventOfCode2020.Day19.day19(sample1)[1] == 2
-#
-#     sample2 = "42: 9 14 | 10 1\n" *
-#               "9: 14 27 | 1 26\n" *
-#               "10: 23 14 | 28 1\n" *
-#               "1: \"a\"\n" *
-#               "11: 42 31\n" *
-#               "5: 1 14 | 15 1\n" *
-#               "19: 14 1 | 14 14\n" *
-#               "12: 24 14 | 19 1\n" *
-#               "16: 15 1 | 14 14\n" *
-#               "31: 14 17 | 1 13\n" *
-#               "6: 14 14 | 1 14\n" *
-#               "2: 1 24 | 14 4\n" *
-#               "0: 8 11\n" *
-#               "13: 14 3 | 1 12\n" *
-#               "15: 1 | 14\n" *
-#               "17: 14 2 | 1 7\n" *
-#               "23: 25 1 | 22 14\n" *
-#               "28: 16 1\n" *
-#               "4: 1 1\n" *
-#               "20: 14 14 | 1 15\n" *
-#               "3: 5 14 | 16 1\n" *
-#               "27: 1 6 | 14 18\n" *
-#               "14: \"b\"\n" *
-#               "21: 14 1 | 1 14\n" *
-#               "25: 1 1 | 1 14\n" *
-#               "22: 14 14\n" *
-#               "8: 42\n" *
-#               "26: 14 22 | 1 20\n" *
-#               "18: 15 15\n" *
-#               "7: 14 5 | 1 21\n" *
-#               "24: 14 1\n\n" *
-#               "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa\n" *
-#               "bbabbbbaabaabba\n" *
-#               "babbbbaabbbbbabbbbbbaabaaabaaa\n" *
-#               "aaabbbbbbaaaabaababaabababbabaaabbababababaaa\n" *
-#               "bbbbbbbaaaabbbbaaabbabaaa\n" *
-#               "bbbababbbbaaaaaaaabbababaaababaabab\n" *
-#               "ababaaaaaabaaab\n" *
-#               "ababaaaaabbbaba\n" *
-#               "baabbaaaabbaaaababbaababb\n" *
-#               "abbbbabbbbaaaababbbbbbaaaababb\n" *
-#               "aaaaabbaabaaaaababaa\n" *
-#               "aaaabbaaaabbaaa\n" *
-#               "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa\n" *
-#               "babaaabbbaaabaababbaabababaaab\n" *
-#               "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba\n"
-#     @test AdventOfCode2020.Day19.day19(sample2)[1] == 3
-#     @test AdventOfCode2020.Day19.day19(sample2)[2] == 12
-# end
+@testset "Day 19" begin
+    sample1 = "0: 4 1 5\n" *
+             "1: 2 3 | 3 2\n" *
+             "2: 4 4 | 5 5\n" *
+             "3: 4 5 | 5 4\n" *
+             "4: \"a\"\n" *
+             "5: \"b\"\n\n" *
+             "ababbb\n" *
+             "bababa\n" *
+             "abbbab\n" *
+             "aaabbb\n" *
+             "aaaabbb\n"
+    @test AdventOfCode2020.Day19.day19(sample1)[1] == 2
+
+    sample2 = "42: 9 14 | 10 1\n" *
+              "9: 14 27 | 1 26\n" *
+              "10: 23 14 | 28 1\n" *
+              "1: \"a\"\n" *
+              "11: 42 31\n" *
+              "5: 1 14 | 15 1\n" *
+              "19: 14 1 | 14 14\n" *
+              "12: 24 14 | 19 1\n" *
+              "16: 15 1 | 14 14\n" *
+              "31: 14 17 | 1 13\n" *
+              "6: 14 14 | 1 14\n" *
+              "2: 1 24 | 14 4\n" *
+              "0: 8 11\n" *
+              "13: 14 3 | 1 12\n" *
+              "15: 1 | 14\n" *
+              "17: 14 2 | 1 7\n" *
+              "23: 25 1 | 22 14\n" *
+              "28: 16 1\n" *
+              "4: 1 1\n" *
+              "20: 14 14 | 1 15\n" *
+              "3: 5 14 | 16 1\n" *
+              "27: 1 6 | 14 18\n" *
+              "14: \"b\"\n" *
+              "21: 14 1 | 1 14\n" *
+              "25: 1 1 | 1 14\n" *
+              "22: 14 14\n" *
+              "8: 42\n" *
+              "26: 14 22 | 1 20\n" *
+              "18: 15 15\n" *
+              "7: 14 5 | 1 21\n" *
+              "24: 14 1\n\n" *
+              "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa\n" *
+              "bbabbbbaabaabba\n" *
+              "babbbbaabbbbbabbbbbbaabaaabaaa\n" *
+              "aaabbbbbbaaaabaababaabababbabaaabbababababaaa\n" *
+              "bbbbbbbaaaabbbbaaabbabaaa\n" *
+              "bbbababbbbaaaaaaaabbababaaababaabab\n" *
+              "ababaaaaaabaaab\n" *
+              "ababaaaaabbbaba\n" *
+              "baabbaaaabbaaaababbaababb\n" *
+              "abbbbabbbbaaaababbbbbbaaaababb\n" *
+              "aaaaabbaabaaaaababaa\n" *
+              "aaaabbaaaabbaaa\n" *
+              "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa\n" *
+              "babaaabbbaaabaababbaabababaaab\n" *
+              "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba\n"
+    @test AdventOfCode2020.Day19.day19(sample2) == [3, 12]
+
+    @test AdventOfCode2020.Day19.day19() == [222, 339]
+end
 
 @testset "Day 20" begin
     sample = "Tile 2311:\n" *
