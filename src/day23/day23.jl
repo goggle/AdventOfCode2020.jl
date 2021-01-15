46542768c7961599cf63a1e4ec84e6f5d0d9f638
Alexander Seiler <seileralex@gmail.com>
2021-01-15 02:48:44 +0100

    [Day 23] Improve Performace
    The cyclic linked list is now implemented as a simple array.

--- src/day23/day23.jl
+++ src/day23/day23.jl
@@ -4,97 +4,65 @@ using AdventOfCode2020
 
 function day23(input::String = readInput(joinpath(@__DIR__, "input.txt")))
     numbers = parse.(Int, split(rstrip(input), ""))
-    list1 = play(numbers, 100)
-    list2 = play(numbers, 10_000_000, extend = true)
-    return [part1(list1), part2(list2)]
+    return [part1(numbers), part2(numbers)]
 end
 
-abstract type AbstractLinkedList{T} end
-abstract type AbstractNode{T} end
-
-mutable struct Node{T} <: AbstractNode{T}
-    next::Node{T}
-    data::T
-    Node{T}() where T =(x=new(); x.next=x; x)
-    Node{T}(n::Node{T}, d::T) where T =new(n, d)
+function part1(numbers)
+    list = play(numbers, 100)
+    arr = similar(list)
+    i = 1
+    for j = 2:length(list)
+        arr[j] = list[i]
+        i = arr[j]
+    end
+    return parse(Int, join(arr[2:end]))
 end
-Node(n::Node{T}, d::T) where T =SListNode{T}(n, d)
 
-mutable struct LinkedList{T} <: AbstractLinkedList{T}
-    node::Node{T}
+function part2(numbers)
+    list = play(numbers, 10_000_000, extend = true)
+    return convert(Int64, list[1]) * list[list[1]]
 end
 
 function play(numbers::Array{Int,1}, N::Int; extend = false)
-    len = 9
-    if extend
+    if !extend
+        len = 9
+        list = Array{Int32,1}(undef, 9)
+    else
         len = 1_000_000
+        list = Array{Int32,1}(undef, 1_000_000)
     end
 
-    nodes = [Node{Int}() for x in numbers]
-    nlist = Array{Node{Int},1}(undef, len)
-    for (i, node) in enumerate(nodes)
-        node.next = nodes[mod1(i + 1, length(nodes))]
-        node.data = numbers[i]
-        nlist[node.data] = node
+    i = numbers[1]
+    for j = 2:9
+        list[i] = numbers[j]
+        i = numbers[j]
     end
-
+    list[i] = numbers[1]
     if extend
-        node = Node{Int}(nodes[1], 1_000_000)
-        nlist[1_000_000] = node
-        @inbounds for i = 999_999:-1:10
-            node = Node{Int}(node, i)
-            nlist[i] = node
-        end
-        nodes[9].next = node
+        list[i] = 10
+        list[10:end] = collect(11:1_000_001)
+        list[end] = numbers[1]
     end
 
-    list = LinkedList{Int}(nodes[1])
-
-    current = list.node
+    current = numbers[1]
     for n = 1:N
-        destval = current.data
-        found = false
-        destination = current
+        destval = current
+        first = list[current]
+        second = list[first]
+        third = list[second]
         while true
             destval = mod1(destval - 1, len)
-            if !(current.next.data == destval || current.next.next.data == destval || current.next.next.next.data == destval)
-                destination = nlist[destval]
+            if destval ∉ (first, second, third)
                 break
             end
         end
-        first = current.next
-        last = current.next.next.next
-        current.next = current.next.next.next.next
-        last.next = destination.next
-        destination.next = first
+        list[current] = list[third]
+        list[third] = list[destval]
+        list[destval] = first
 
-        current = current.next
+        current = list[current]
     end
     return list
-end
-
-function part1(list::LinkedList{Int})
-    onenode = list.node
-    while onenode.data != 1
-        onenode = onenode.next
-    end
-    c = onenode.next
-    res = []
-    while c.data != 1
-        push!(res, c.data)
-        c = c.next
-    end
-    return parse(Int, join(res))
-end
-
-function part2(list::LinkedList{Int})
-    onenode = list.node
-    while onenode.data != 1
-        onenode = onenode.next
-    end
-    one = onenode.next
-    two = one.next
-    return one.data * two.data
 end
 
 end  # module
